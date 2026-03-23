22b6bbc11059fbfac2262b77928c153a7506dfde
marcelodeus98 <marcelodeus98@gmail.com>
2026-03-23 00:19:06 -0300

    add EnvConfigModule and EnvConfigService for environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfigInterface } from './env-config-interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfigInterface {
+  constructor(
+    private configService: ConfigService
+  ){}
+
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV') || 'development' as string;
+  }
+
+  getPort(): number {
+    return Number(this.configService.get<number>('PORT') || 3000);
+  }
+}
